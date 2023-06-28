6eea1745437cba048b8b275c9c2f526853c587ad
GabrielPatricioSantos <123960853+GabrielPatricioSantos@users.noreply.github.com>
2023-06-28 13:15:05 -0300

    Projeto

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function ShowMenu () {
+    let menu = document.querySelector('.lista_menu')
+
+    if (menu.classList.contains('open')) {
+        
+        menu.classList.remove('open')
+    } else {
+        menu.classList.add('open')
+    }
+}
+    
